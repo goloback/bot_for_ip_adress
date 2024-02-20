06198f4a38707e7561860221d2f2e2642204ae5c
goloback <adduma2008@gmail.com>
2024-02-20 16:16:00 +0200

    дороботка фунции get_info_by_ip

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ import requests
 def get_info_by_ip(ip='156.202.176.194'):
     try:
         resours = requests.get(url=f'http://ip-api.com/json/{ip}').json()
-        print(resours)
+        #print(resours)
+        info={
+            '[IP]': resours.get('query'),
+            '[Int prov]': resours.get('isp'),
+            '[Org]': resours.get('org'),
+            '[Country]': resours.get('country'),
+            '[Region Name]': resours.get('regionName'),
+            '[City]': resours.get('city'),
+            '[ZIP]': resours.get('zip'),
+            '[Lat]': resours.get('lat'),
+            '[Lon]': resours.get('lon'),
+        }
+        for k, v in info.items():
+            print(f'{k}: {v}')
     except requests.exceptions.ConnectionError:
         print('please check your connection')
 
